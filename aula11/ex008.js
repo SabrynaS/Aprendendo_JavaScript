b5fa02cbcd052b84d175141a642ddc639e61baa8
SabrynaS <130665935+SabrynaS@users.noreply.github.com>
2023-04-20 10:48:25 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel=82
+
+console.log(`Sua velcidade é ${vel}km/h`)
+if(vel>60){
+    console.log("Velocidade acima do permitido! MULTADO")
+}else{
+    console.log("Velocidade dentro do permitido")
+}
+
